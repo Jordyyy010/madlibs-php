05f1b6ad1652be0a74b97344a13a635a5a6be496
MSI\Jordy Clement <99044408@mydavinci.nl>
2018-02-02 13:51:43 +0100

    klaar

--- index.php
+++ index.php
@@ -1,38 +1,31 @@
+
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Madlibs</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet">
   </head>
   <body>
-      <h1>Mad Libs</h1>
+    <h1>Mad Libs</h1>
     <article>
-      Er heerst paniek...
-      Onkunde
+      <a href="index.html">Er heerst paniek...</a>
+      <a href="onkunde.html">Onkunde</a>
     </article>
     <main>
-      <form action="onkunde.php" method="post">
-      <aside>
-        <input type="text" name="name">
-        <input type="text" name="persoon">
-        <input type="text" name="land">
-        <input type="text" name="verveel">
-        <input type="text" name="speelgoed">
-        <input type="text" name="spijbel">
-        <input type="text" name="kopen">
-        <input type="text" name="bezigheid">
-        <button type="submit">Verstuur</button>
-      </aside>
-        <p>Welk dier zou je nooit als huisdier willen hebben?</p>
-        <p>Wie is de belangrijkste persoon in je leven?</p>
-        <p>In welk land zou je graag willen wonen?</p>
-        <p>Wat doe je als je je verveelt?</p>
-        <p>Met welk speelgoed speelde je als kind het meest?</p>
-        <p>Bij welke docent spijbel je het liefst?</p>
-        <p>Als je € 100.000,- had, wat zou je dan kopen?</p>
-        <p>Wat is je favoriete bezigheid?</p>
-      </form>
+      <h2>Er heerst paniek...</h2>
+      <p>Er heerst paniek in het koninkrijk <?php echo $_POST['land'];?>.
+      Koning <?php echo $_POST['spijbel']; ?> is ten einde raad en als koning <?php echo $_POST['spijbel'];?> ten einde raad is,
+      dan roept hij zijn ten-einde-raadsheer <?php echo $_POST['persoon']; ?>.</p>
+      <p>"<?php echo $_POST['persoon']; ?>! Het is een ramp! Het is een schande!"</p>
+      <p>"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"</p>
+      <p>"Mijn <?php echo $_POST['name']; ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST['speelgoed']; ?> voor hem gekocht!"</p>
+      <p>"Majesteit, uw <?php echo $_POST['name']; ?> komt vast vanzelf terug?"</p>
+      <p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST['bezigheid']; ?> leren?"</p>
+      <p>"Maar Sire, daar kunt u toch uw <?php echo $_POST['kopen']; ?> voor gebruiken."</p>
+      <p>"<?php echo $_POST['persoon']; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."</p>
+      <p>"<?php echo $_POST['verveel']; ?>, Sire."</p>
     </main>
     <footer>
       <p>Deze website is gemaakt door <?php echo $_POST['spijbel'];?></p>
--- onkunde.php
+++ onkunde.php
@@ -5,26 +5,20 @@
     <meta charset="utf-8">
     <title>Madlibs</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet">
   </head>
   <body>
     <h1>Mad Libs</h1>
     <article>
-      Er heerst paniek...
-      Onkunde
+      <a href="index.html">Er heerst paniek...</a>
+      <a href="">Onkunde</a>
     </article>
     <main>
-      <h2>Er heerst paniek...</h2>
-      <p>Er heerst paniek in het koninkrijk <?php echo $_POST['land'];?>.
-      Koning <?php echo $_POST['spijbel']; ?> is ten einde raad en als koning <?php echo $_POST['spijbel'];?> ten einde raad is,
-      dan roept hij zijn ten-einde-raadsheer <?php echo $_POST['persoon']; ?>.</p>
-      <p>"<?php echo $_POST['persoon']; ?>! Het is een ramp! Het is een schande!"</p>
-      <p>"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"</p>
-      <p>"Mijn <?php echo $_POST['name']; ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST['speelgoed']; ?> voor hem gekocht!"</p>
-      <p>"Majesteit, uw <?php echo $_POST['name']; ?> komt vast vanzelf terug?"</p>
-      <p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST['bezigheid']; ?> leren?"</p>
-      <p>"Maar Sire, daar kunt u toch uw <?php echo $_POST['kopen']; ?> voor gebruiken."</p>
-      <p>"<?php echo $_POST['persoon']; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."</p>
-      <p>"<?php echo $_POST['verveel']; ?>, Sire."</p>
+      <h2>Onkunde</h2>
+      <p>Er zijn veel mensen die niet kunnen <?php echo $_POST['specialiteit'];?>. Neem nou <?php echo $_POST['opschieten'];?>. Zelfs met de hulp<br>
+        van een <?php echo $_POST['vakantie'];?> of zelfs <?php echo $_POST['getal'];?> kan <?php echo $_POST['opschieten'];?> niet  <?php echo $_POST['specialiteit'];?>. Dat heeft niet te maken met<br>
+        een gebrek aan <?php echo $_POST['eigenschap'];?>, maar met een te veel aan <?php echo $_POST['slechtste'];?>. Te veel <?php echo $_POST['slechtste'];?><br>
+        leidt tot <?php echo $_POST['overkomen'];?>. en dat is niet goed als je wilt leren <?php echo $_POST['specialiteit'];?>.. Helaas voor <?php echo $_POST['opschieten'];?></p>
     </main>
     <footer>
       <p>Deze website is gemaakt door <?php echo $_POST['spijbel'];?></p>
